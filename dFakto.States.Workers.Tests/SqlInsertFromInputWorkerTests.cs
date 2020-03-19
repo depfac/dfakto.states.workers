c7d5b15ede66a7046dbdd25f635c20eb97ef5316
Vincent DARON <vda@dfakto.com>
2020-03-19 14:18:25 +0100

    Improve unit tests

--- dFakto.States.Workers.Tests/SqlInsertFromInputWorkerTests.cs
+++ dFakto.States.Workers.Tests/SqlInsertFromInputWorkerTests.cs
@@ -11,7 +11,7 @@ namespace dFakto.States.Workers.Tests
     public class SqlInsertFromInputWorkerTests : BaseTests
     {
         [Fact]
-        public async Task TestScalarQuery()
+        public async Task TestSqlInsertFromInputWorker()
         {
             var sql = Host.Services.GetService<SqlInsertFromInputWorker>();
 
@@ -22,24 +22,55 @@ namespace dFakto.States.Workers.Tests
                 var input = new SqlInsertFromInputWorkerInput();
                 input.ConnectionName = database.Name;
                 input.TableName = tableName;
-                input.Values = new Dictionary<string, object>(){{"col1",23},{"col2","COUCOU"}};
+                input.Values = new Dictionary<string, object>(){{"col1",23},{"col2","COUCOU"},{"col3",null}};
                 var output = await sql.DoJsonWork<SqlInsertFromInputWorkerInput,string>(input);
                 
+                Assert.Equal(1,CountTable(database,tableName));
+                
+                DropTable(database, tableName);
             }
         }
         
         private string CreateTable(BaseDatabase database)
         {
             var tableName = StringUtils.Random(10);
-            var conn = database.CreateConnection();
+            using var conn = database.CreateConnection();
             conn.Open();
             using (var cmd = conn.CreateCommand())
             {
-                cmd.CommandText = $"CREATE TABLE {tableName} (col1 INT , col2 VARCHAR(100))";
+                cmd.CommandText = $"CREATE TABLE {tableName} (col1 INT,col2 VARCHAR(100),col3 VARCHAR(100))";
                 cmd.ExecuteNonQuery();
             }
 
             return tableName;
+        }
+        
+        private void DropTable(BaseDatabase database, string tableName)
+        {
+            using var conn = database.CreateConnection();
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"DROP TABLE {tableName}";
+                cmd.ExecuteNonQuery();
+            }
+        }
+        
+        private long CountTable(BaseDatabase database, string tableName)
+        {
+            using var conn = database.CreateConnection();
+            conn.Open();
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"SELECT COUNT(*) FROM {tableName}";
+                var r = cmd.ExecuteScalar();
+                if (r is long)
+                {
+                    return (long) r;
+                }
+
+                return (int) r;
+            }
         }
     }
 }
