6ef6b0abd9d17bf3258b0d0096acc3559953b505
Vincent DARON <vda@dfakto.com>
2020-03-26 10:15:15 +0100

    Remove commented code

--- dFakto.States.Workers/BaseWorker.cs
+++ dFakto.States.Workers/BaseWorker.cs
@@ -7,29 +7,6 @@ using dFakto.States.Workers.Interfaces;
 
 namespace dFakto.States.Workers
 {
-    // class JsonConverterJsonElement : JsonConverter<JsonElement>
-    // {
-    //     public override JsonElement Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    //     {
-    //         using (JsonDocument document = JsonDocument.ParseValue(ref reader))
-    //         {
-    //             return document.RootElement.Clone();
-    //         }
-    //     }
-    //
-    //     public override void Write(Utf8JsonWriter writer, JsonElement value, JsonSerializerOptions options)
-    //     {
-    //         if (value.ValueKind == JsonValueKind.Undefined)
-    //         {
-    //             writer.WriteNullValue();
-    //         }
-    //         else
-    //         {
-    //             value.WriteTo(writer);
-    //         }
-    //     }
-    // }
-    
     public abstract class BaseWorker<TI,TO> : IWorker
     {
         private readonly JsonSerializerOptions _options = new JsonSerializerOptions
